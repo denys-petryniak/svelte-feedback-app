5c1af00e2cb6f9b2c819484bddba9a62821f22ab
J3m1th <denys.petryniak@gmail.com>
2022-10-08 17:20:19 +0300

    Create FeedbackList & FeedbackItem & Card components

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    position: relative;
+    padding: 40px 50px;
+    margin: 20px 0;
+    color: #333;
+    background-color: #fff;
+    border-radius: 15px;
+  }
+</style>
